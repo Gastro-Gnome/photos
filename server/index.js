1712ce327a18cbd16df2494f4aaac4a9050fe05c
zjthomas <zachthomas809@gmail.com>
2019-07-03 19:42:13 -0700

    able to launch server get static files

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('../public'))
+
 app.get('/', (req, res) => {
-  res.end();
+  res.end('hi');
 });
 
 app.listen(port, () => console.log(`Photos now listening on port ${port}`));
