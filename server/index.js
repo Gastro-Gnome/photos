f6f5a993aa536b106b36c3718b2f8172d6dece89
zjthomas <zachthomas809@gmail.com>
2019-07-07 22:06:01 -0700

    successful api request

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Photos = require('../database/photos.js')
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,12 @@ app.use(express.static('../public'));
 
 app.get('/', (req, res) => {
   res.end('hi');
+});
+
+app.get('/biz_photos/:businessId', (req, res) => {
+  Photos.find({ business_id: req.params.businessId })
+    .then(data => res.send(JSON.stringify(data)))
+    .catch(err => res.send(JSON.stringify(err)));
 });
 
 app.listen(port, () => console.log(`Photos now listening on port ${port}`));
