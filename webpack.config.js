3a840249ad8fbc4a24f4e9051478e93d5e03744d
zjthomas <zachthomas809@gmail.com>
2019-07-08 18:21:40 -0700

    serve static files

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,20 @@ module.exports = {
   entry: './client/index.jsx',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
-        test: /\.m?(js|jsx)$/,
+        test: /.(js|jsx)$/,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
 };
