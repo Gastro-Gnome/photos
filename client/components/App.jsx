51551b6c2c2818a8659c092903c96dc0a6d0dd97
zjthomas <zachthomas809@gmail.com>
2019-07-10 10:03:36 -0700

    rebase

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import PhotoCarousel from './PhotoCarousel.jsx';
 
-<<<<<<< HEAD
-const App = () => (
-  <div>
-   Hello
-  </div>
-);
-=======
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,16 +16,15 @@ class App extends React.Component {
     return (
       <div className="showcase-container">
         <div className="showcase-container_inner">
-          {/* <div className="top-shelf-grey" /> */}
           <PhotoCarousel />
           {/* <div className="nav nav-left" />
           <div className="nav nav-right" /> */}
         </div>
-        <div className="photos-footer">Footer</div>
+        <div className="photos-footer" />
       </div>
     );
   }
 }
->>>>>>> setup basic layout for service
+
 
 export default App;
